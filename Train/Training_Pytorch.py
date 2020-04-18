37424ae1c72d3603b2fcb79bb32c0a491dd2d9d3
Maoyang <61926510+xmy0053@users.noreply.github.com>
2020-04-18 20:20:27 +0800

    Create Training_Pytorch.py

---
+++ Train/Training_Pytorch.py
@@ -0,0 +1,271 @@
+'''ResNet in PyTorch.
+For Pre-activation ResNet, see 'preact_resnet.py'.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+import os
+import torch.optim as optim
+import functools
+BATCH_SIZE =5
+Mode = 1
+
+data_transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), std =(0.5, 0.5, 0.5)),
+])
+
+class Conv2dAuto(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.padding =  (self.kernel_size[0] // 2, self.kernel_size[1] // 2) # dynamic add padding based on the kernel_size
+
+conv3x3 = functools.partial(Conv2dAuto, kernel_size=3, bias=False)    
+def conv_bn(in_channels, out_channels, conv, *args, **kwargs):
+    return nn.Sequential(conv(in_channels, out_channels, *args, **kwargs), nn.BatchNorm2d(out_channels))
+def activation_func(activation):
+    return  nn.ModuleDict([
+        ['relu', nn.ReLU(inplace=True)],
+        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+        ['selu', nn.SELU(inplace=True)],
+        ['none', nn.Identity()]
+    ])[activation]
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, activation='relu'):
+        super().__init__()
+        self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
+        self.blocks = nn.Identity()
+        self.activate = activation_func(activation)
+        self.shortcut = nn.Identity()   
+    
+    def forward(self, x):
+        residual = x
+        if self.should_apply_shortcut: residual = self.shortcut(x)
+        x = self.blocks(x)
+        x += residual
+        x = self.activate(x)
+        return x
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.out_channels
+
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=conv3x3, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1,
+                      stride=self.downsampling, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)) if self.should_apply_shortcut else None
+        
+        
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
+
+class ResNetBasicBlock(ResNetResidualBlock):
+    """
+    Basic ResNet block composed by two layers of 3x3conv/batchnorm/activation
+    """
+    expansion = 1
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, conv=self.conv, bias=False, stride=self.downsampling),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.expanded_channels, conv=self.conv, bias=False),
+        )
+
+
+
+class ResNetLayer(nn.Module):
+    """
+    A ResNet layer composed by `n` blocks stacked one after the other
+    """
+    def __init__(self, in_channels, out_channels, block=ResNetBasicBlock, n=1, *args, **kwargs):
+        super().__init__()
+        # 'We perform downsampling directly by convolutional layers that have a stride of 2.'
+        downsampling = 2 if in_channels != out_channels else 1
+        self.blocks = nn.Sequential(
+            block(in_channels , out_channels, *args, **kwargs, downsampling=downsampling),
+            *[block(out_channels * block.expansion, 
+                    out_channels, downsampling=1, *args, **kwargs) for _ in range(n - 1)]
+        )
+
+    def forward(self, x):
+        x = self.blocks(x)
+        return x
+
+class ResNetEncoder(nn.Module):
+    """
+    ResNet encoder composed by layers with increasing features.
+    """
+    def __init__(self, in_channels=3, blocks_sizes=[64, 128, 256, 512], deepths=[2,2,2,2], 
+                 activation='relu', block=ResNetBasicBlock, *args, **kwargs):
+        super().__init__()
+        self.blocks_sizes = blocks_sizes
+        
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_channels, self.blocks_sizes[0], kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.blocks_sizes[0]),
+            activation_func(activation),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        
+        self.in_out_block_sizes = list(zip(blocks_sizes, blocks_sizes[1:]))
+        self.blocks = nn.ModuleList([ 
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], activation=activation, 
+                        block=block,*args, **kwargs),
+            *[ResNetLayer(in_channels * block.expansion, 
+                          out_channels, n=n, activation=activation, 
+                          block=block, *args, **kwargs) 
+              for (in_channels, out_channels), n in zip(self.in_out_block_sizes, deepths[1:])]       
+        ])
+        
+        
+    def forward(self, x):
+        x = self.gate(x)
+        for block in self.blocks:
+            x = block(x)
+        return x
+
+class ResnetDecoder(nn.Module):
+    """
+    This class represents the tail of ResNet. It performs a global pooling and maps the output to the
+    correct class by using a fully connected layer.
+    """
+    def __init__(self, in_features, n_classes):
+        super().__init__()
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
+        self.decoder = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.decoder(x)
+        return x
+class ResNetBottleNeckBlock(ResNetResidualBlock):
+    expansion = 4
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, expansion=4, *args, **kwargs)
+        self.blocks = nn.Sequential(
+           conv_bn(self.in_channels, self.out_channels, self.conv, kernel_size=1),
+             activation_func(self.activation),
+             conv_bn(self.out_channels, self.out_channels, self.conv, kernel_size=3, stride=self.downsampling),
+             activation_func(self.activation),
+             conv_bn(self.out_channels, self.expanded_channels, self.conv, kernel_size=1),
+        )
+
+class ResNet(nn.Module):
+    
+    def __init__(self, in_channels, n_classes, *args, **kwargs):
+        super().__init__()
+        self.encoder = ResNetEncoder(in_channels, *args, **kwargs)
+        self.decoder = ResnetDecoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, n_classes)
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+def resnet18(in_channels, n_classes, block=ResNetBasicBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[2, 2, 2, 2], *args, **kwargs)
+
+def resnet34(in_channels, n_classes, block=ResNetBasicBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
+
+def resnet50(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
+
+def resnet101(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 23, 3], *args, **kwargs)
+
+def resnet152(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 8, 36, 3], *args, **kwargs)
+
+def test():
+    net = ResNetEncoder()
+    y = net(torch.randn(1,3,32,32))
+    print(y.size())
+    
+
+def main():
+# Set Training data
+    train_dataset = datasets.ImageFolder(root="dataset-resized", transform=data_transform)
+    train_dataloader = DataLoader(dataset=train_dataset,
+                                  batch_size=BATCH_SIZE,
+                                  shuffle=True,
+                                  num_workers=4)
+    device = torch.device('cuda:0')
+    if Mode == 1:
+        if os.path.exists("ResNet18.pth"):
+            net = torch.load('ResNet18.pth')
+        else :
+            net = resnet18(3, 4)
+        net.to(device)
+        params = list(net.parameters())
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        lossl=[]
+        for epoch in range(200):
+            running_loss = 0.0
+            for i, data in enumerate(train_dataloader,0):
+                # get the input
+                inputs, labels = data
+                # print(i,labels)
+                # zeros the paramster gradients
+                inputs = inputs.to(device)      
+                labels = labels.to(device)      
+                optimizer.zero_grad()       #
+                # forward + backward + optimize
+                outputs = net(inputs)
+                # print(outputs)
+                # print(labels)
+                # quit()
+                loss = criterion(outputs, labels)  # 
+                # print(outputs, labels)
+                # quit()
+                loss.backward()     # loss 
+                optimizer.step()    # 
+                # print statistics 
+                # print(i)
+                running_loss += loss.item()  # tensor.item()  
+                if i % 200 == 199:
+                    #print(outputs, labels)
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 200))  # 
+                    lossl.append(running_loss / 200)
+                    running_loss = 0.0
+                    os.system("echo off")
+                    torch.save(net, 'ResNet18.pth')
+                    os.system("echo on")
+                    dummy_input = torch.randn(1,3, 224, 224,device='cuda')#, 
+                    torch.onnx.export(net, dummy_input, "ResNet.onnx", verbose=True)
+        print(lossl)
+        print('Finished Training')
+    else:
+        net = torch.load('model_cfair10_2.pth')
+        net.eval()
+        dummy_input = torch.randn(1,3, 224, 224,device='cuda')#, 
+        torch.onnx.export(net, dummy_input, "ResNet.onnx", verbose=True)
+        # for data in train_dataloader:
+        #     I,L= data
+        #     I=I.to(device)
+        #     L=L.to(device)
+        #     print("Predict",net(I))
+        #     print("Label",L)
+
+if __name__ == '__main__':
+    main()
